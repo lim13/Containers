cd6ae21dbff8551fa1622143171582b484a69cb2
Clarinda Dwyynen <cdwyynen@at-m8.msk.21-school.ru>
2022-08-08 21:19:44 +0300

    rev_it

---
+++ reverse_iterator.hpp
@@ -0,0 +1,21 @@
+#ifndef FT_ITERATOR
+# define FT_ITERATOR
+
+#include <iterator>
+
+namespace ft{
+    
+    template <class Iterator> 
+    class reverse_iterator 
+    {   
+        public:
+            typedef Iterator iterator_type;
+		    typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+		    typedef typename iterator_traits<Iterator>::value_type value_type;
+		    typedef typename iterator_traits<Iterator>::difference_type	difference_type;
+		    typedef typename iterator_traits<Iterator>::pointer pointer;
+		    typedef typename iterator_traits<Iterator>::reference reference;
+    };
+}
+
+# endif
