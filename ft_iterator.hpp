3e2081b97bf77e92426e886a10f1172e90ec860e
Clarinda Dwyynen <cdwyynen@at-m8.msk.21-school.ru>
2022-08-08 20:31:51 +0300

    iterator_traits_done_3

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -4,6 +4,12 @@
 #include <iterator>
 
 namespace ft{
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag : public input_iterator_tag {};
+    struct bidirectional_iterator_tag : public forward_iterator_tag {};
+    struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
     template <class Iterator> struct iterator_traits {
         typedef typename Iterator::iterator_category iterator_category;
         typedef typename Iterator::value_type value_type;
